dca4df2cb9150d133361d6a717b6b7c54febb64b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-28 14:05:14 -0500

    added arithmetic js

---
+++ variables/arithmetic.js
@@ -0,0 +1,9 @@
+var a = 5;
+var b = 7;
+var sum = a + b;
+var subtraction = a - b;
+var mul = a * b;
+var div = a / b;
+var modulus = a % b;
+
+document.body.innerHTML = "<h3> sum = " + sum + " </h3>";
---
+++ variables/script.js
@@ -0,0 +1,4 @@
+var number = 5;
+var negNumber = -6;
+var isBoolean = true;
+var name = "Saul";
