e16fa226f90744418a9083f52bec48098aa81234
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-28 14:10:59 -0500

    added logical js

---
+++ logical/logical.js
@@ -0,0 +1,14 @@
+var a = 5;
+var b = 6;
+
+if(a == b) {
+	console.log(a + " == to " + b);
+} else {
+	console.log(a + " != to " + b);
+}
+
+if(a > b) {
+	console.log(a + " is > than " + b);
+} else {
+	console.log(b + " is < than " + a);
+}
