a449e05afff52d9338e2c010a1e2c39cf3c69891
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-28 14:28:30 -0500

    added arrays js

---
+++ arrays/arrays.js
@@ -0,0 +1,11 @@
+//pop, shift, unshift, other methods, etc...
+
+var colors;
+colors = ["red", "green", "yellow"];
+
+console.log(colors);
+console.log(colors[0]);
+console.log("length = " + colors.length);
+console.log("reversed = " + colors.reverse());
+colors.shift();
+console.log(colors);
--- logical/logical.js
+++ logical/logical.js
@@ -1,5 +1,11 @@
 var a = 5;
-var b = 6;
+var b = 5;
+var isEqual = (a == b);
+var isGreaterThan = (a > b);
+var isLessThan = (a < b);
+var isNotEqualTo = (a != b);
+
+//AND/OR
 
 if(a == b) {
 	console.log(a + " == to " + b);
@@ -12,3 +18,6 @@ if(a > b) {
 } else {
 	console.log(b + " is < than " + a);
 }
+
+//ternary
+a == b ? console.log("match exists") : console.log("Match does not exist");
